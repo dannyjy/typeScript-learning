6a25424e37495f8a574f82ad96d70edb57e12fe5
dannyjy <danieljohn9295@gmail.com>
2026-03-01 14:34:34 +0200

    feat: add the checkCoupon function

---
+++ Codewars/checkCoupon.ts
@@ -0,0 +1,5 @@
+function checkCoupon(enteredCode: string, correctCode: string, currentDate: string, expirationDate: string): boolean {
+  return enteredCode === correctCode && new Date(currentDate) <= new Date(expirationDate);
+}
+
+console.log(checkCoupon('123', '123', 'September 5, 2014', 'October 1, 2014'))
